8dea575ed01cb322e6f89f6d1132a4f70ce562da
bcajes <brian.cajes@gmail.com>
2012-11-09 18:13:32 -0500

    cleanup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     packages=find_packages(),
     url='https://github.com/bcajes/makosg/',
     license='LICENSE.txt',
-    install_requires=['sendgrid-python', 'mako']
+    install_requires=['sendgrid-python', 'mako'],
     description='makosg makes rendering rich html for sendgrid emails easier',
     long_description='Works in conjunction with sendgrid-python.  Given mako templated text, some variables, and/or mako files, render richer email content easily.',
 )
