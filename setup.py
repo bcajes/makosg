fd49c154f771922e143771b612cac9ca7c38e81c
Brian Cajes <brian.cajes@gmail.com>
2012-11-10 15:42:16 -0500

    updating meta

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='makosg',
-    version='0.0.1',
+    version='0.0.2',
     author='Brian Cajes',
     author_email='brian.cajes@gmail.com',
     packages=find_packages(),
     url='https://github.com/bcajes/makosg/',
-    license='LICENSE.txt',
+    license='MIT License',
     install_requires=['sendgrid-python', 'mako'],
     description='Easily render rich html emails for sendgrid using mako templates',
     long_description='Works in conjunction with sendgrid-python.  Given mako templated text, some variables, and/or mako files, render richer email content easily.',
